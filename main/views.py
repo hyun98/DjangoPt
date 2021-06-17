a173679a10c9642264109b532fb930750e06a534
Henu <hyun0404woo@gmail.com>
2021-06-18 00:45:46 +0900

    django course 8 commit

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def hello_world(request):
-    return HttpResponse("hello world!")
+    return render(request, 'base.html')
