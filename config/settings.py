9dfdbf67305a114347b8f001014cdf80c31e8ec8
Henu <hyun0404woo@gmail.com>
2021-06-18 03:14:30 +0900

    django course 9 commit

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'accountapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
