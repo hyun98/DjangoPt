0efbd9c003c4d1748a70d5d78191bddc2eb2011d
Henu <hyun0404woo@gmail.com>
2021-06-23 11:58:12 +0900

    media root setting

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -6,15 +6,7 @@ def account_ownership_required(func):
     def decorated(request, *args, **kwargs):
         user = User.objects.get(pk=kwargs['pk'])
         if not user == request.user:
-<<<<<<< HEAD
-            return render(request)
-        
-    
-    
-    
-=======
             return HttpResponseForbidden()
         return func(request, *args, **kwargs)
 
->>>>>>> 0336da6090023bfd94adec0375bcf27d0ec82c3f
     return decorated
