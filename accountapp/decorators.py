a7eeb975e7a5c1eb56b3c8708169f9e619bf7ea5
Henu <hyun0404woo@gmail.com>
2021-06-23 11:39:04 +0900

    install pillow

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -4,7 +4,7 @@ def decorator(func):
     def decorated(request, *args, **kwargs):
         user = User.objects.get(pk==kwargs['pk'])
         if not user == request.user:
-            return HttpR
+            return render(request)
         
     
     
