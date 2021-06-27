fdd651a26345d194265c95fdc6f2ae121231aeb9
Henu <hyun0404woo@gmail.com>
2021-06-27 16:33:32 +0900

    multipleobjectmixin

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,11 +7,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import *
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
 
@@ -23,10 +26,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    
+    paginate_by = 25
+    
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list)
 
 
 @method_decorator(has_ownership, 'get')
--- articleapp/models.py
+++ articleapp/models.py
@@ -3,8 +3,12 @@ from django.db import models
 
 
 # Create your models here.
+from projectapp.models import Project
+
+
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='article')
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name='article')
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
